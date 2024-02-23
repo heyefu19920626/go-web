2b592b8b2993b66d47141c9bc186b5815b7ce9e8
tangan <527380349@qq.com>
2024-02-23 23:07:45 +0800

    1、logrus同时写入文件与控制台

--- main.go
+++ main.go
@@ -5,13 +5,47 @@ import (
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"go-web/cli"
+	"io"
+	"os"
 )
 
 func main() {
-	logrus.SetFormatter(&LogFormatter{})
-	logrus.Info("Hello word!")
+	if initLogConfig() {
+		return
+	}
 	localCli := cli.LocalCli{}
 	localCli.ExecCommand("dir")
+}
+
+func initLogConfig() bool {
+	logrus.SetFormatter(&LogFormatter{})
+	logPath := "log/all.log"
+	_, err := os.Stat(logPath)
+	if err != nil {
+		logrus.Infof("logfile not exits, will create")
+		_, err := os.Stat("log")
+		if err != nil {
+			logrus.Infof("log dir not exits, will create")
+			err := os.Mkdir("log", os.ModePerm)
+			if err != nil {
+				logrus.Errorf("create log dir error. %+v", err)
+				return true
+			}
+			_, err = os.Stat(logPath)
+			if err != nil {
+				_, err := os.Create(logPath)
+				if err != nil {
+					logrus.Errorf("create logfile error. %+v", err)
+					return true
+				}
+			}
+			logrus.Infof("logfile create success")
+		}
+	}
+	logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	writer := io.MultiWriter(os.Stdout, logFile)
+	logrus.SetOutput(writer)
+	return false
 }
 
 // LogFormatter 自定义logrus的日志格式
